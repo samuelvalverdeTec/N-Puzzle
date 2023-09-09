095abfaa1520ad59fe9c4ee6dc6239dab5d8b0a9
samuelvalverdee <savalverde@estudiantec.cr>
2023-09-08 19:51:46 -0600

    base

---
+++ n-puzzle.js
@@ -0,0 +1,5 @@
+//  Proyecto 1 - N-Puzzle Game
+//  Samuel Valverde Arguedas & Erick Kauffmann Porcar
+//  Instituto Tecnológico de Costa Rica, sede Cartago
+//  Análisis de Algoritmos - GR 03
+//  II Semestre, 2023
