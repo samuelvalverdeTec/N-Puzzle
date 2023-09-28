e7fb8eba1847ce1d7187d08e61f074bf7240ce9a
samuelvalverdee <savalverde@estudiantec.cr>
2023-09-27 23:27:56 -0600

    interfaz avance 1

--- n-puzzle.js
+++ n-puzzle.js
@@ -3,3 +3,14 @@
 //  Instituto Tecnológico de Costa Rica, sede Cartago
 //  Análisis de Algoritmos - GR 03
 //  II Semestre, 2023
+
+function createBoard(n) {
+    var result = []
+    for(var i = 0; i < n; i++) {
+        result.push(new Array(n).fill(0))
+    }
+    return result
+}
+
+var board = createBoard(5)
+console.log(board)
